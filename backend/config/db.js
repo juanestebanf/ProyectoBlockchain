79dd74711bc64492899c833f7b58a107049982ee
juanestebanf <juanestebanfuentes30@gmail.com>
2026-06-28 01:52:18 -0500

    Creación carpeta config - validar conexión

---
+++ backend/config/db.js
@@ -0,0 +1,23 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+// Verificar conexión
+pool.connect()
+    .then(client => {
+        console.log("✅ PostgreSQL conectado correctamente");
+        client.release();
+    })
+    .catch(err => {
+        console.error("❌ Error al conectar PostgreSQL");
+        console.error(err.message);
+    });
+
+module.exports = pool;
