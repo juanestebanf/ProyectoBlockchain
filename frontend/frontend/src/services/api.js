b16089f8b61ed1c11f27f1160aac802a2120c39f
juanestebanf <juanestebanfuentes30@gmail.com>
2026-08-07 01:37:45 -0500

    Actualizar URL del backend

--- frontend/frontend/src/services/api.js
+++ frontend/frontend/src/services/api.js
@@ -1,16 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+
+    baseURL: "https://proyectoblockchain.onrender.com/api"
+
 });
 
-// Agregar automáticamente el token
 api.interceptors.request.use((config) => {
 
     const token = localStorage.getItem("token");
 
     if (token) {
+
         config.headers.Authorization = `Bearer ${token}`;
+
     }
 
     return config;
